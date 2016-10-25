7ae886dfe9582898e509d104c8587d36eaf6c0d8
niquola <niquola@gmail.com>
2016-10-25 06:23:43 +0300

    deploy to clojars

--- README.md
+++ README.md
@@ -2,17 +2,20 @@
 
 [![Build Status](https://travis-ci.org/niquola/clj-pg.svg)](https://travis-ci.org/niquola/clj-pg)
 
+[![Clojars Project](https://img.shields.io/clojars/v/clj-pg.svg)](https://clojars.org/clj-pg)
+
 [![Dependency Status](https://www.versioneye.com/user/projects/56461327b5b03d002200085a/badge.svg?style=flat)](https://www.versioneye.com/user/projects/56461327b5b03d002200085a)
 
-A Clojure library designed to ... well, that part is up to you.
+
+A repeated code for clojure and postgresql
 
 ## Usage
 
-FIXME
+See tests
 
 ## License
 
-Copyright © 2015 FIXME
+Copyright © 2015 niquola
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,20 +1,23 @@
-(defproject clj_pg "0.1.0-SNAPSHOT"
+(defproject clj-pg "0.0.1-RC1"
   :description "Collection of helpers to work with pg from clj"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [honeysql "0.6.2"]
-                 [clj-time "0.11.0"]
-                 [environ "1.0.1"]
+  :repositories   [["clojars"  {:url "https://clojars.org/repo" :sign-releases false}]]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [honeysql "0.8.1"]
+                 [clj-time "0.12.0"]
+                 [environ "1.1.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [com.zaxxer/HikariCP "2.4.3"]
+                 [com.zaxxer/HikariCP "2.5.1"]
                  [org.postgresql/postgresql "9.4.1208.jre7"]
                  [org.clojure/java.jdbc "0.5.0"]
-                 [cheshire "5.5.0"]
+                 [cheshire "5.6.3"]
                  [functionalbytes/mount-lite "0.9.8"]]
 
   :source-paths  ["src" "test"]
 
-  :profiles {:dev {:dependencies  [[ch.qos.logback/logback-classic "1.1.3"]]}})
+  :profiles {:dev {:plugins [[lein-ancient "0.6.10"]] 
+                   :dependencies  [[ch.qos.logback/logback-classic "1.1.7"]]}})
